d07ef3c7ef35ff53855e5aa89c12a414e4d7a6a7
Zakaria Mahmud <zakaria.mahmud@qlik.com>
2016-04-18 10:06:42 +0200

    first test
    My first test with chai and mocha

---
+++ test/first.js
@@ -0,0 +1,15 @@
+/**
+ * Created by zma on 2016-04-18.
+ */
+
+var expect= require('chai').expect;
+
+describe ('A basic test', function(){
+
+    it('is should pass when everything is ok', function(){
+
+       expect(true).to.be.true;
+
+    });
+
+});
